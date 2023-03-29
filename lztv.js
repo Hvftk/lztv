f1f2ec31d1427694191ed1db0bcb59f8197e617b
Hvftk <57172166+Hvftk@users.noreply.github.com>
2023-03-29 11:03:10 +0800

    Update lztv.js

--- lztv.js
+++ lztv.js
@@ -1,4 +1,8 @@
+/*
 var body = $response.body
     .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Hvftk/lztv@main/lztv.css" type="text/css">')
     .replace(/jquerys.js\?v/g, "ddgksf2013.js?v");
+$done({ body });
+*/
+var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Hvftk/lztv@main/lztv.css" type="text/css">').replace(/'ddjhcvtk'/g, "'112233'");
 $done({ body });
